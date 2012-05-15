a38718de8e1bac8d4f5dcf8f738aa8d65929545e
Marc Schwieterman <marc@marcisme.com>
2012-05-14 23:13:06 -0400

    try increased polling timeout for travis

--- features/support/guard_helper.rb
+++ features/support/guard_helper.rb
@@ -11,6 +11,8 @@ module GuardHelper
   end
 
   def verify_guard_behavior(max_tries = 10)
+    # try increasing timeout for travis
+    max_tries *= 5 if ENV['TRAVIS_RUBY_VERSION']
     tries = 0
     begin
       in_current_dir { yield }
