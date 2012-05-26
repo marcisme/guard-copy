ffec744e0b4e058fd0ddafd271652cf35686d609
Marc Schwieterman <marc@marcisme.com>
2012-05-25 23:05:54 -0400

    increase test polling timeout

--- features/support/guard_helper.rb
+++ features/support/guard_helper.rb
@@ -3,14 +3,15 @@ require 'aruba/api'
 module GuardHelper
 
   GUARD_CMD = 'guard start'
+  POLL_INTERVAL = 0.1
 
   def start_guard(guardfile_contents)
     write_file('Guardfile', guardfile_contents)
     run_interactive(unescape(GUARD_CMD))
-    sleep 0.1 until output_from(GUARD_CMD).include?('Guard is now watching')
+    sleep POLL_INTERVAL until output_from(GUARD_CMD).include?('Guard is now watching')
   end
 
-  def verify_guard_behavior(max_tries = 20)
+  def verify_guard_behavior(max_tries = 50)
     # try increasing timeout for travis
     max_tries *= 5 if ENV['TRAVIS']
     tries = 0
@@ -18,7 +19,7 @@ module GuardHelper
       in_current_dir { yield }
     rescue => e
       if (tries += 1) < max_tries
-        sleep 0.1
+        sleep POLL_INTERVAL
         retry
       else
         raise e
