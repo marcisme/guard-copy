fd796552bb78f9693784cc4f888d5d680a9902cb
Marc Schwieterman <marc@marcisme.com>
2012-11-19 19:12:25 -0500

    [re]disable output during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,4 +34,16 @@ end
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers
   config.include FileHelpers
+
+  config.before(:each) do
+    # Setting GUARD_ENV to 'test' used to disable output, but the stubbing below
+    # (copied from guard's spec_helper) seems to be the way to do it now.
+
+    # Stub all UI methods, so no visible output appears for the UI class
+    ::Guard::UI.stub(:info)
+    ::Guard::UI.stub(:warning)
+    ::Guard::UI.stub(:error)
+    ::Guard::UI.stub(:debug)
+    ::Guard::UI.stub(:deprecation)
+  end
 end
